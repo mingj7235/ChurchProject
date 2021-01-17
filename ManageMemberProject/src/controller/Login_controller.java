b11011e3bc66d5f0c5020d7206071bd5ae172000
mingJ7235 <joshuara7235@gmail.com>
2021-01-17 22:32:38 +0900

    Project commit for church

--- ManageMemberProject/src/controller/Login_controller.java
+++ ManageMemberProject/src/controller/Login_controller.java
@@ -1,16 +1,27 @@
 package controller;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Login_controller implements Initializable{
 	
-	
+	@FXML private TextField login_id_textfield;
+	@FXML private Label login_current_date;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("a HH:mm");
+		
+		login_current_date.setText(sdf.format(date));
 		
 	}
 
