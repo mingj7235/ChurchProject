33b2ef45c0e7cd4418e99668455c95ba0ea340a9
mingJ7235 <joshuara7235@gmail.com>
2021-01-17 18:06:41 +0900

    Modified login fxml files

---
+++ ManageMemberProject/src/controller/Login_controller.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Login_controller implements Initializable{
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
