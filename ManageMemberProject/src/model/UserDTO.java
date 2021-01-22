3da4dea1f96252ff9dc3b2fb6951519e7fcc8d7d
mingJ7235 <joshuara7235@gmail.com>
2021-01-22 17:29:51 +0900

    Create userdao, userdto

---
+++ ManageMemberProject/src/model/UserDTO.java
@@ -0,0 +1,46 @@
+package model;
+
+public class UserDTO {
+	
+	private String name; //이름
+	private String phonenum; //전화번호
+	private String age; //또래
+	private String grade; //비고 (출석에따라 등급)
+	private String chief;	//또래장
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhonenum() {
+		return phonenum;
+	}
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getChief() {
+		return chief;
+	}
+	public void setChief(String chief) {
+		this.chief = chief;
+	}
+	
+	
+	
+	
+}
