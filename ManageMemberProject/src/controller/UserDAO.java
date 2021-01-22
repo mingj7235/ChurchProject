e42333677197b4f08bd21b2c9641baff73fcb992
mingJ7235 <joshuara7235@gmail.com>
2021-01-22 22:38:58 +0900

    Modify UserDto

--- ManageMemberProject/src/controller/UserDAO.java
+++ ManageMemberProject/src/controller/UserDAO.java
@@ -1,5 +1,28 @@
 package controller;
 
-public class UserDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import model.UserDTO;
+
+public class UserDAO {
+	
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	//청년부 등록
+	
+	public void signUp (UserDTO dto) {
+		
+		
+	}
+	
+	//청년부 수정
+	
+	//청년부 삭제
+	
+	//청년부 조회
+	
 }
